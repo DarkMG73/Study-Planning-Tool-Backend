7ca79692cc49dd81856cc732a2ca2d864d3ae383
DarkMG73 <levelthreeemail@gmail.com>
2024-04-07 19:00:46 -0400

    v1.0.0| Fixed registration message.

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -56,7 +56,6 @@ module.exports.register = asyncHandler(async (req, res) => {
       user.hash_password = undefined;
       return res.json(user);
     })
-
     .catch((err) => {
       if (err.name === "MongoError" && err.code === 11000) {
         // Duplicate username
