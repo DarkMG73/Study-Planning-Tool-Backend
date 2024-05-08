b157c2e1e89aeac66d8b9a5f0a8c95d2850055be
DarkMG73 <levelthreeemail@gmail.com>
2024-05-07 21:27:29 -0400

    2024-05-08T01:27:29.621Z:

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -17,7 +17,7 @@ const exitIfDemoUser = (user_id) => {
   return demoUser.includes(user_id);
 };
 // The httpOnly cookie expires in 1 day.
-const expirationTime = { expiresIn: "1d" };
+const expirationTime = { expiresIn: 10 };
 
 ////////////////////////////////
 /// Handlebars Config
